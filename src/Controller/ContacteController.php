e888dd14878c61bd2d10bd921fe04c467c3731e7
Patryk333 <kowalikpatryk02@gmail.com>
2025-12-12 13:03:13 +0100

    Teoria tema 3

--- src/Controller/ContacteController.php
+++ src/Controller/ContacteController.php
@@ -1,78 +1,63 @@
 <?php
-
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 // Definir una ruta base i nom (prefix) a nivell de controlador
 #[Route('/contactes', name: 'contacte_')]
-class ContacteController
+class ContacteController extends AbstractController
 {
     // Llista de contactes de mostra
-    private $contactes = [
-        ["codi" => 1, "nom" => "Salvador Sala", 
+    private array $contactes = [
+        ["codi" => 1, "nom" => "Salvador Sala",
          "telefon" => "638961244", "email" => "salvasala@simarro.org"],
-        ["codi" => 2, "nom" => "Anna Llopis", 
+        ["codi" => 2, "nom" => "Anna Llopis",
          "telefon" => "669332004", "email" => "annallopis@simarro.org"],
-        ["codi" => 3, "nom" => "Marc Sanchis", 
+        ["codi" => 3, "nom" => "Marc Sanchis",
          "telefon" => "962286040", "email" => "marcsanchis@simarro.org"],
-        ["codi" => 4, "nom" => "Laura Palop", 
+        ["codi" => 4, "nom" => "Laura Palop",
          "telefon" => "663568890", "email" => "laurapalop@simarro.org"],
     ];
 
-    // Métode 1: Mostrar la fitxa d'un contacte pel seu codi
-    // Cada mètode defineix la seua ruta relativa i el seu mètode HTTP
+    // Métode 1
     #[Route('/{codi}', name:'fitxa', methods: ['GET'], requirements: ['codi' => '\d+'])]
     public function fitxa(int $codi): Response
     {
-        $resultat = array_filter($this->contactes, 
-            function($contacte) use ($codi){
-                return $contacte['codi'] == $codi;
-            }
-        );
+        // 1) Buscar el contacte pel codi
+        $resultat = array_filter($this->contactes, fn($c) => $c['codi'] === $codi);
 
-        if (!$resultat) 
-            return new Response('Contacte no trobat');
+        if (!$resultat) {
+            // En un cas real, podríem llançar una 404 o mostrar una vista d’error
+            return $this->render('contacte/fitxa.html.twig', [
+                'contacte' => null,
+                'codi' => $codi,
+            ]);
+        }
 
-        // Torna 1r element
         $contacte = array_shift($resultat);
-        $resp = "<ul>
-                    <li>{$contacte['nom']}</li>
-                    <li>{$contacte['telefon']}</li>
-                    <li>{$contacte['email']}</li>
-                </ul>";
 
-        return new Response("<html><body>$resp</body></html>");
+        // 2) Passar dades a la plantilla Twig
+        return $this->render('contacte/fitxa.html.twig', [
+            'contacte' => $contacte,
+            'codi' => $codi,
+        ]);
     }
 
-
-    // Métode 2: Buscar contactes pel seu nom
-    // Mateixa ruta /contacte/{text}, però amb requirement diferent (només lletres)
+    // Métode 2
     #[Route('/{text}', name: 'buscar', methods: ['GET'], requirements: ['text' => '[a-zA-Z]+'])]
     public function buscar(string $text): Response
     {
-        $resultat = array_filter($this->contactes, 
-            function($contacte) use ($text) {
-                return stripos($contacte["nom"], $text) !== false;
-            }
-        );
+        // 1) Filtrar per nom (coincidència parcial, sense tindre en compte maj./min.)
+        $resultat = array_filter($this->contactes, function ($c) use ($text) {
+            return stripos($c['nom'], $text) !== false;
+        });
 
-        if (!$resultat) 
-            return new Response("No s'han trobat contactes amb '$text'.");
-
-
-        $resposta = "<h2>Resultats de la cerca: '$text'</h2>";
-        foreach ($resultat as $contacte) {
-            $resposta .= "<ul>
-                            <li>{$contacte['nom']}</li>
-                            <li>{$contacte['telefon']}</li>
-                            <li>{$contacte['email']}</li>
-                          </ul>";
-        }
-
-        return new Response("<html><body>$resposta</body></html>");
-
+        // 2) Passar la llista (pot ser buida) a la vista
+        return $this->render('contacte/cerca.html.twig', [
+            'text' => $text,
+            'resultats' => $resultat,
+        ]);
     }
 }
-?>
--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IniciController
+class IniciController extends AbstractController
 {
     #[Route('/', name: 'inici', methods: ['GET'])]
     public function inici(): Response
     {
-        return new Response('Benvingut/da al web de contactes');
+        return $this->render('inici.html.twig');
     }
+    
 }
+
 ?>
